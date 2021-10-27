9b560840ce9c170958fa644638444c6d628ad317
CelestineSutanto <Celestine.Sutanto@education.nsw.gov.au>
2021-10-28 10:59:58 +1100

    05 Final

--- 05forLoop.py
+++ 05forLoop.py
@@ -5,3 +5,5 @@ print ('|    Name : Celestine Sutanto                    |')
 print ('|    Version : 01                                |')
 print ('|                                                |')
 print (' ------------------------------------------------ ')
+for x in range(1, 500):
+    print (x)
