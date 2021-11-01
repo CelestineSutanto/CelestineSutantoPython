df6a59f0507cd0b05aaa130402ed132a8fbc10e3
CelestineSutanto <Celestine.Sutanto@education.nsw.gov.au>
2021-11-01 18:48:23 +1100

    06 Final

---
+++ 06whileLoop.py
@@ -0,0 +1,22 @@
+print (' ------------------------------------------------ ')
+print ('|                                                |')
+print ('|    06whileLoop                                 |') 
+print ('|    Name : Celestine Sutanto                    |')
+print ('|    Version : 01                                |')
+print ('|                                                |')
+print (' ------------------------------------------------ ')
+subject = input ('What is the name of this subject ')
+while subject != 'IST':
+    print ('Not Correct - try again')
+    subject = input ('What is the name of this subject ')
+    continue
+else:
+    print ('')
+    print ('')
+    print (' Congratulations!!')
+    print ('')
+    print ('')
+    
+    
+    
+        
