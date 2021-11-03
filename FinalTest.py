443b1b8e4c7fc3698d9740f5873330a5b301b8de
CelestineSutanto <Celestine.Sutanto@education.nsw.gov.au>
2021-11-03 22:32:26 +1100

    07, Final Version

---
+++ FinalTest.py
@@ -0,0 +1,169 @@
+import sys
+
+def options():
+    print (' ------------------------------------------------ ')
+    print ('|                                                |')
+    print ('|    Final Version                               |') 
+    print ('|    Name : Celestine Sutanto                    |')
+    print ('|    Version : 01                                |')
+    print ('|                                                |')
+    print (' ------------------------------------------------ ')
+    print ('')
+    print ('1. Hello World')
+    print ('2. Goodbye World')
+    print ('3. Goodbye Person')
+    print ('4. Good Teacher')
+    print ('5. forLoop')
+    print ('6. whileLoop')
+    print ('7. string Loop')
+    print ('8. Convert to ascii')
+    print ('9. Encode a string')
+    print ('x. To Exit')
+
+def helloworld():
+    print ('')
+    print ('----Start of Output ---------------------------')
+    print ('')
+    print ('Hello World')
+    print ('')
+    print ('----End of Output -----------------------------')
+
+def goodbyeworld():
+    print ('')
+    print ('----Start of Output ---------------------------')
+    print ('')
+    print ('Hello World')
+    x = input('------> Program paused - press enter to continue')
+    print('Goodbye World')
+    print ('')
+    print ('----End of Output -----------------------------')
+
+def goodbyeperson():
+    print ('')
+    print ('----Start of Output ---------------------------')
+    print ('')
+    print ('Hello World')
+    x = input('What is your name ? ')
+    print('Goodbye ' + x)
+    print ('')
+    print ('----End of Output -----------------------------')
+
+def goodteacher():
+    print ('')
+    print ('----Start of Output ---------------------------')
+    print ('')
+    x = input('Teacher\'s name (try Mr Horan) ')
+    if x == "Mr Horan":
+        print ('You are lucky, he is a great teacher.')
+    else:
+        print (x + ' is an ok teacher')
+    print ('')
+    print ('----End of Output -----------------------------')
+
+def forloop():
+    print ('')
+    print ('----Start of Output ---------------------------')
+    print ('')
+    for x in range(1, 500):
+        print (x)
+    print ('')
+    print ('----End of Output -----------------------------')
+
+def whileloop():
+    print ('')
+    print ('----Start of Output ---------------------------')
+    subject = input ('What is the name of this subject ')
+    while subject != 'IST':
+        print ('Not Correct - try again')
+        subject = input ('What is the name of this subject ')
+        continue
+    else:
+        print ('')
+        print ('')
+        print (' Congratulations!!')
+        print ('')
+        print ('')
+        print ('')
+    print ('----End of Output -----------------------------')
+
+def stringloop():
+    print ('')
+    print ('----Start of Output ---------------------------')
+    print ('')
+    string = input ('What is your string? ')
+    for x in string:
+        print (x)
+    print ('')
+    print ('----End of Output -----------------------------')
+
+def ascii():
+    print ('')
+    print ('----Start of Output ---------------------------')
+    print ('')
+    string = input ('What is your string? ')
+    for x in string:
+        print (x + '='+ str (ord(x)))
+    print ('')
+    print ('----End of Output -----------------------------')
+
+def encodestring():
+    print ('')
+    print ('----Start of Output ---------------------------')
+    print ('')
+    string = input ('What is your string? ')
+    previous = ''
+    for x in string:
+        next = chr (ord(x)+1)
+        print (x + '='+ next)
+        print (previous + next)
+        previous = previous + next
+    print ('')
+    print ('----End of Output -----------------------------')
+
+while True:
+    options()
+    menu = input ('Enter an option ')
+    valid = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "x"]
+
+    if menu in valid:
+        if menu == '1':
+            helloworld()
+        elif menu == '2':
+            goodbyeworld()
+        elif menu == '3':
+            goodbyeperson()
+        elif menu == '4':
+            goodteacher()
+        elif menu == '5':
+            forloop()
+        elif menu == '6':
+            whileloop()
+        elif menu == '7':
+            stringloop()
+        elif menu == '8':
+            ascii()
+        elif menu == '9':
+            encodestring()
+        elif menu == 'x':
+            print ('')
+            print ('----Start of Output ---------------------------')
+            print ('')
+            print ('')
+            print ('----End of Output -----------------------------')
+            print ('')
+            print ('')
+            print ('')
+            input ('Press Enter to continue')
+            print ('')
+            sys.exit()
+    else:
+        print ('----Start of Output ---------------------------')
+        print ('')
+        print ('invalid option')
+        print ('')
+        print ('----End of Output -----------------------------')
+        
+    print ('')
+    print ('')
+    print ('')
+    input ('Press Enter to continue')
